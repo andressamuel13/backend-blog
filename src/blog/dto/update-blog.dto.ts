a739204f319b16a083f94553df69a2627d19e70a
andressamuel13 <samuelma236@gmail.com>
2025-06-26 20:54:39 -0500

    creado el dto de actualizar

--- src/blog/dto/update-blog.dto.ts
+++ src/blog/dto/update-blog.dto.ts
@@ -1,4 +1,29 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBlogDto } from './create-blog.dto';
+import { IsOptional, IsString, MinLength } from "class-validator";
 
-export class UpdateBlogDto extends PartialType(CreateBlogDto) {}
+export class UpdateBlogDto {
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    title?: string;
+    
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    description?: string;
+    
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    content?: string;
+    
+    @IsString()
+    @IsOptional()
+    author?: string;
+    
+    @IsString()
+    @IsOptional()
+    category?: string;
+    
+    @IsOptional()
+    date?: Date;
+}
